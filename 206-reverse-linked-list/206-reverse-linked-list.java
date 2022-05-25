65770c5ad88fc24a41702407531fdd81d100bfff
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-05-25 18:40:02 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (90.96%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -26,3 +26,25 @@ class Solution {
        return reverse(curr, head);
     }
 }
+/*
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        
+        ListNode res=null;
+        
+        ListNode prev=res;
+        
+        while(head!=null){
+            
+            ListNode curr=new ListNode(head.val);
+            
+            curr.next=prev;
+            prev=curr;
+            head=head.next;
+        }
+        return prev;
+        
+    }
+}
+*/
