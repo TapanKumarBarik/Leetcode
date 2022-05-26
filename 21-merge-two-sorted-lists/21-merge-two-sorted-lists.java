8814723a3c2f57c272009698d7f3630d4d03c2a6
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-05-26 18:35:12 +0530

    Time: 1 ms (65.67%), Space: 42.8 MB (62.36%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -11,34 +11,25 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-        
+      /*  
         if(list1==null)return list2;
         if(list2==null)return list1;
-        
         ListNode head=new ListNode(0);
-        
         ListNode curr=head;
-        
         while(list1!=null && list2!=null){
-            
             if(list1.val>list2.val){
-                
                 ListNode temp=new ListNode(list2.val);
                 list2=list2.next;
-                
                 curr.next=temp;
                 curr=curr.next;
             }
-            
             else if(list1.val<list2.val){
                   ListNode temp=new ListNode(list1.val);
                 list1=list1.next;
-                
                 curr.next=temp;
                 curr=curr.next;
             }
             else{
-                
                 ListNode temp=new ListNode(list1.val);
                 ListNode temp1=new ListNode(list2.val);
                 list1=list1.next;
@@ -47,23 +38,27 @@ class Solution {
                 curr=curr.next;
                 curr.next=temp1;
                 curr=curr.next;
-            }
-            
+            }   
         }
-        
-        
-        //
-         
-        if(list1!=null ){        
+        if(list1!=null ){    
                 curr.next=list1;
-
         }
-        
         if(list2!=null ){        
            curr.next=list2;
-
         }
-         
         return head.next;
+        
+        */
+        
+        if(list1==null)return list2;
+        if(list2==null)return list1;
+        if(list1.val<list2.val){
+            list1.next=mergeTwoLists( list1.next,  list2);
+            return list1;
+        }
+        else{
+             list2.next=mergeTwoLists( list1,  list2.next);
+            return list2;
+        }
     }
 }
