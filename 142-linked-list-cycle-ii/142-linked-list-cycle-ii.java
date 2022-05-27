9866e16fe41077bce6e9ee7635c0b6f352f51f96
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-05-27 07:48:10 +0530

    Time: 1 ms (41.20%), Space: 45 MB (47.00%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -12,7 +12,27 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         
-         if(head==null || head.next==null){return null;}
+       /*  if(head==null || head.next==null){return null;}
+        ListNode fast=head;
+        ListNode slow=head;
+        
+        while(  slow!=null && fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+               break;
+            }
+        }
+        if(slow!=fast)return null;
+        slow=head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+        */
+        
+            if(head==null || head.next==null){return null;}
         ListNode fast=head;
         ListNode slow=head;
         
@@ -31,4 +51,4 @@ public class Solution {
         
         return null;
     }
-}
+    }
