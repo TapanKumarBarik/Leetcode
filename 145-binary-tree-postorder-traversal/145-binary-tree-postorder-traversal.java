dece64fd8fe04e0756d99aacda3a923601473af1
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-05-28 21:06:20 +0530

    Time: 1 ms (44.41%), Space: 41.7 MB (67.28%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -14,10 +14,30 @@
  * }
  */
 class Solution {
+    
     public List<Integer> postorderTraversal(TreeNode root) {
         
+     
+      	LinkedList<Integer> ans = new LinkedList<>();
+	Stack<TreeNode> stack = new Stack<>();
+	if (root == null) return ans;
+	
+	stack.push(root);
+	while (!stack.isEmpty()) {
+		TreeNode cur = stack.pop();
+		ans.addFirst(cur.val);
+		if (cur.left != null) {
+			stack.push(cur.left);
+		}
+		if (cur.right != null) {
+			stack.push(cur.right);
+		} 
+	}
+	return ans;
+    }
         
-        List<Integer>list=new ArrayList();
+        
+    /*    List<Integer>list=new ArrayList();
         
         solve(list, root);
         
@@ -32,5 +52,6 @@ class Solution {
         solve(list, root.right);
         list.add(root.val);
         
-    }
+    }*/
+    
 }
