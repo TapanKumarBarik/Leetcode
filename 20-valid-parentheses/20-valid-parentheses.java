67f385459b08f43a59bc59b011bfe83cafb839eb
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-04 13:48:03 +0530

    Time: 3 ms (49.40%), Space: 42.1 MB (38.19%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character>st=new Stack();
+        
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            
+            if(s.charAt(i)=='('){
+                st.push(')');
+            }
+            else  if(s.charAt(i)=='{'){
+                st.push('}');
+            }
+            else  if(s.charAt(i)=='['){
+                st.push(']');
+            }
+            else if(st.isEmpty() || st.pop()!=s.charAt(i)){
+                return false;
+            }
+        }
+        
+        return st.isEmpty();
+    }
+}
