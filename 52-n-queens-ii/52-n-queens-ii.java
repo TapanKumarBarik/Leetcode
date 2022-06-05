70608894f877626fa38009e43ea4cb53f05a03bf
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-05 07:50:18 +0530

    Time: 7 ms (32.17%), Space: 41.8 MB (26.91%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -8,14 +8,20 @@ public int totalNQueens(int n) {
 
 private int totalNQueensHelper(int row, int count, int n) {
     for (int col = 0; col < n; col++) {
-        if (occupiedCols.contains(col))
-            continue;
+        if (occupiedCols.contains(col)){
+              continue;
+        }
+          
         int diag1 = row - col;
-        if (occupiedDiag1s.contains(diag1))
-            continue;
+        if (occupiedDiag1s.contains(diag1)){
+             continue;
+        }
+           
         int diag2 = row + col;
-        if (occupiedDiag2s.contains(diag2))
-            continue;
+        if (occupiedDiag2s.contains(diag2)){
+             continue;
+        }
+           
         // we can now place a queen here
         if (row == n-1)
             count++;
