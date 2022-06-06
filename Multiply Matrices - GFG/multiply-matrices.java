2915b44bac78293f4befb12514036150307ca030
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-06 19:51:16 +0530

    Added solution - LeetHub

--- Multiply Matrices - GFG/multiply-matrices.java
+++ Multiply Matrices - GFG/multiply-matrices.java
@@ -37,7 +37,7 @@ class GfG
            {
                for(int j=0;j<N;j++)
                {
-                   //C[i][j]=0;
+                   
                    for(int k=0;k<N;k++)
                    {
                        C[i][j]+=A[i][k]*B[k][j];
