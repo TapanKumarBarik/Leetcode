13a77785c15a778129b87ebb7925ba58f483bfef
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-09 14:30:26 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (55.38%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -1,5 +1,11 @@
 class Solution {
     public int getSum(int a, int b) {
-        	return (b == 0) ? a : getSum(a ^ b, (a & b) << 1);
+        	//recursion
+        
+            int xor=a^b;
+        int carry=a&b;
+        if(carry==0)return xor;
+        
+        return getSum(xor, carry<<1);
     }
 }
