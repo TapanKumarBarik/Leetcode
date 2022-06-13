596a95673aa6131893948c558a3989601120b80f
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-13 07:56:57 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.java
+++ Reverse a String - GFG/reverse-a-string.java
@@ -1,33 +1,45 @@
 // { Driver Code Starts
-// Initial template for Java
+//Initial Template for Java
 
 import java.util.*;
 import java.io.*;
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String S = read.readLine();
-            Solution ob = new Solution();
+import java.lang.*;
 
-            System.out.println(ob.revStr(S));
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
         }
     }
 }// } Driver Code Ends
 
 
-class Solution {
-    static String revStr(String S) {
-        // code here
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
         
-        int n=S.length()-1;
-        String res="";
+        StringBuilder sb=new StringBuilder();
+        int n=str.length()-1;
+        
         for(int i=n;i>=0;i--){
-            res+=S.charAt(i);
+            
+            sb.append(str.charAt(i) );
+            
         }
-        
-        return res;
+        return sb.toString();
     }
 }
