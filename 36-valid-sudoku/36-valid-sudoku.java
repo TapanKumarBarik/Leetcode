9313cd84b4b1fe11ab285b4a0dd5f6c967e72abf
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-13 20:25:03 +0530

    Time: 28 ms (12.57%), Space: 47.4 MB (42.16%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -5,9 +5,9 @@ class Solution {
         for (int j=0; j<9; ++j) {
             char number = board[i][j];
             if (number != '.'){
-                if (!seen.add(number + " in row " + i) ||
-                    !seen.add(number + " in column " + j) ||
-                    !seen.add(number + " in block " + i/3 + "-" + j/3))
+                if (!seen.add(number + "in row" + i) ||
+                    !seen.add(number + "in column" + j) ||
+                    !seen.add(number + "in block" + i/3 + "-" + j/3))
                 {
                      return false;
                 }
