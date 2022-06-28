c72369a0d67fe11dd668f7c54a1b7ded3771b3f4
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-06-28 11:41:44 +0530

    Time: 27 ms (16.33%), Space: 46.6 MB (18.02%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,43 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        int n=s.length();
-        
-        int m=t.length();
-        
-        if(m!=n)return false;
-        
+        if(s.length()!=t.length())return false;
         HashMap<Character, Integer>map=new HashMap();
         
-        for(int i=0;i<n;i++){
+        //store first value in map
+        for(int i=0;i<s.length();i++){
             
             char curr=s.charAt(i);
-            
             if(map.containsKey(curr)){
                 int freq=map.get(curr);
-                map.put(curr, freq+1);
+                map.put(curr,freq+1);
             }
             else{
-                map.put(curr, 1);
+              map.put(curr,1);   
             }
         }
         
         //
-        for(int i=0;i<n;i++){
-           char curr=t.charAt(i); 
+        for(int i=0;i<t.length();i++){
+             char curr=t.charAt(i);
             if(map.containsKey(curr)){
                 int freq=map.get(curr);
-                map.put(curr, freq-1);
-                
+                map.put(curr,freq-1);
                 if(freq==1){
                     map.remove(curr);
                 }
             }
             else{
-              return false;
+             return false; 
             }
+            
         }
         
-        return true;
+        return map.size()==0?true:false;
     }
 }
