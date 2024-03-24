e2846556a6f9118653f0d1d5d3f160fdf401f0a3
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2024-03-24 09:41:13 +0530

    Time: 1 ms (97.25%), Space: 45.8 MB (21.45%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,44 +1,62 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         
-        //1,7,3,6,5,6
-        //1,8,11,17,22,28
-        //28,27,20,17,11,6
+     //Approach 1
         
-        //[2,1,-1]
+//         int n=nums.length;
         
-       // 2,3,2
-        //2,0,-1
+//         int[]prefix=new int[n];
         
-        int n=nums.length;
+//         int[]suff=new int[n];
         
-        int[]prefix=new int[n];
+//         int sum=0;
         
-        int[]suff=new int[n];
+//         for(int i=0;i<n;i++){
+            
+//             sum+=nums[i];
+//             prefix[i]=sum;
+            
+//         }
+        
+//         sum=0;
+//         for(int i=n-1;i>=0;i--){
+//             sum+=nums[i];
+//             suff[i]=sum;
+            
+//         }
+        
+//         for(int i=0;i<n;i++){
+            
+//             if(prefix[i]==suff[i]){
+//                 return i;
+//             }
+//         }
+        
+        
+//         return -1;
+        
+        
+        
+        
+        //ApprocaH 2 
         
         int sum=0;
         
-        for(int i=0;i<n;i++){
-            
-            sum+=nums[i];
-            prefix[i]=sum;
-            
-        }
-        
-        sum=0;
-        for(int i=n-1;i>=0;i--){
-            sum+=nums[i];
-            suff[i]=sum;
-            
-        }
+        int n=nums.length;
         
         for(int i=0;i<n;i++){
             
-            if(prefix[i]==suff[i]){
+            sum+=nums[i];
+        }
+        
+         int leftIndexSum=0;
+        for(int i=0;i<n;i++){
+            
+            if(leftIndexSum==sum-leftIndexSum-nums[i]){
                 return i;
             }
+            leftIndexSum+=nums[i];
         }
-        
         
         return -1;
     }
